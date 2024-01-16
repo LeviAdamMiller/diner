f4d7063f87a25cf35afc1b977ed1a4c41426702a
Levi Miller <Miller.Levi@student.greenriver.edu>
2024-01-16 12:57:03 -0500

    added fatfree

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
-echo "hello";
+
+/*
+ * Levi Miller
+ * January 2024
+ * https://levimiller.greenriverdev.com/328/diner/
+ * this is my controller for the Diner app
+ */
+
+
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+//require the autoload file
+require_once ('vendor/autoload.php');
+
+//instantiate Fat-Free framework
+$f3 = Base::instance();
+
+//define default route
+$f3 ->route('GET /',function(){
+    echo "My Diner";
+});
+//run fat free
+$f3->run();
+
+
