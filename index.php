adc21eec38e7c2b6a1a6b229e458a7b4bc436897
Levi Miller <Miller.Levi@student.greenriver.edu>
2024-01-31 17:21:33 -0500

    added routes to order form 1 and 2 and summary also added those pages to view directory

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
-
 /*
  * Levi Miller
  * January 2024
  * https://levimiller.greenriverdev.com/328/diner/
  * this is my controller for the Diner app
  */
-
-
 
 // turn on error reporting
 ini_set('display_errors', 1);
@@ -20,15 +17,79 @@ require_once ('vendor/autoload.php');
 //instantiate Fat-Free framework
 $f3 = Base::instance();
 
-//define default route
-$f3 ->route('GET /breakfast',function(){
-   //echo "Breakfast";
-
+//define route
+$f3 ->route('GET /',function(){
     //display views page
-    $view = new template();
-    echo $view->render('views/breakfast-menu.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
-//run fat free
+
+//define breakfast route
+$f3 ->route('GET /breakfast',function(){
+    // display views page
+     $view = new Template();
+     echo $view->render('views/breakfast-menu.html');
+});
+// Define a order form 1 route
+$f3->route('GET|POST /order1', function($f3) {
+   // echo "Order Form Part I";
+   $view = new Template();
+    echo $view->render('views/order-form-1.html');
+// vid explains this code 1:30 hr 1/23/24
+    // If the form has been posted
+   if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // Validate the data
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+
+        // Put the data in the session array
+        $f3->set('SESSION.food', $food);
+        $f3->set('SESSION.meal', $meal);
+
+        // Redirect to order2 route
+       $f3->reroute('order2');
+   }
+});
+
+    // Define a order form 2 route
+    $f3->route('GET|POST /order2', function($f3) {
+     //echo "Order Form Part II";
+
+     // If the form has been posted
+      if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // Validate the data
+        if (isset($_POST['conds'])){
+          $conds = implode(", ", $_POST['conds']);
+       }
+       else {
+           $conds = "None selected";
+      }
+
+       // Put the data in the session array
+          $f3->set('SESSION.conds', $conds);
+
+       // Redirect to summary route
+       $f3->reroute('summary');
+
+    }
+
+    // Display a view page
+   $view = new Template();
+   echo $view->render('views/order-form-2.html');
+
+});
+
+// Define an order summary route
+   $f3->route('GET /summary', function() {
+    //echo "Thank you for your order!";
+
+    // Display a view page
+   $view = new Template();
+  echo $view->render('views/order-summary.html');
+});
+
 $f3->run();
 
 
